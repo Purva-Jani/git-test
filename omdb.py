6a0827a9367e697063cae593e387c4d0bd1eb3f3
Purva-Jani <66566447+Purva-Jani@users.noreply.github.com>
2022-12-14 08:24:50 -0500

    pulling movie info attempt

--- omdb.py
+++ omdb.py
@@ -6,3 +6,4 @@ from omdbapi.movie_search import GetMovie
 userInp = input("what's your favorite movie?\n")
 movie = GetMovie(api_key="ebaa4487")
 print(movie.get_movie(userInp))
+# print(movie.get_data('genre', 'language', 'imdbrating'))
