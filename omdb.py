6ef345c0f8ee402203f8e8049f24b4725b7cdecd
Purva-Jani <66566447+Purva-Jani@users.noreply.github.com>
2022-12-14 08:19:30 -0500

    collecting user input

--- omdb.py
+++ omdb.py
@@ -3,5 +3,6 @@
 import omdbapi
 from omdbapi.movie_search import GetMovie
 
+userInp = input("what's your favorite movie?\n")
 movie = GetMovie(api_key="ebaa4487")
-print(movie.get_movie("Interstellar"))
+print(movie.get_movie(userInp))
