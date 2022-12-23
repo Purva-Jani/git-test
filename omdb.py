d153f0da44e53f38a838d43f10c6c6eebb087b34
ChanRao1 <120386957+ChanRao1@users.noreply.github.com>
2022-12-23 09:45:13 -0500

    Update omdb.py

--- omdb.py
+++ omdb.py
@@ -3,7 +3,9 @@
 import omdbapi
 from omdbapi.movie_search import GetMovie
 
-userInp = input("what's your favorite movie?\n")
+clientInp = input("what's your favorite movie?\n")
 movie = GetMovie(api_key="ebaa4487")
-print(movie.get_movie(userInp))
+print(movie.get_movie(clientInp))
 # print(movie.get_data('genre', 'language', 'imdbrating'))
+# movie.get_data('director', 'actors', 'awards', 'plot')
+
