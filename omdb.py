ff1b6ea4a91a194ee1347561014e62945c215009
ChanRao1 <120386957+ChanRao1@users.noreply.github.com>
2022-12-23 09:52:20 -0500

    Update omdb.py

--- omdb.py
+++ omdb.py
@@ -7,5 +7,10 @@ clientInp = input("what's your favorite movie?\n")
 movie = GetMovie(api_key="ebaa4487")
 print(movie.get_movie(clientInp))
 # print(movie.get_data('genre', 'language', 'imdbrating'))
-# movie.get_data('director', 'actors', 'awards', 'plot')
+# print(movie.get_data('director', 'actors', 'awards', 'plot'))
+
+def getMovieRecommendations():
+    movieName = clientInp
+    
+    
 
